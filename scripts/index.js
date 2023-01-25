1875a1ba6718925aa60680873f63a91e03b209d6
Stepan Skvortsov <skvorstieven@yandex.ru>
2023-01-25 12:26:27 +0300

    feat: добавить начальные карточки через js

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,47 @@
-let profile = document.querySelector('.profile');
-let profileName = profile.querySelector('.profile__title');
-let profileJob = profile.querySelector('.profile__subtitle');
-let editProfileButton = profile.querySelector('.button_type_edit');
+// Объявление переменных
 
-let editProfilePopUp = document.querySelector('.popup');
-let editProfileForm = editProfilePopUp.querySelector('.popup__form');
-let exitEditProfilePopUp = editProfilePopUp.querySelector('.button_type_exit');
-let nameInput = editProfilePopUp.querySelector('.popup__input_type_name');
-let jobInput = editProfilePopUp.querySelector('.popup__input_type_job');
+const profile = document.querySelector('.profile');
+const profileName = profile.querySelector('.profile__title');
+const profileJob = profile.querySelector('.profile__subtitle');
+const editProfileButton = profile.querySelector('.button_type_edit');
 
+const editProfilePopUp = document.querySelector('.popup');
+const editProfileForm = editProfilePopUp.querySelector('.popup__form');
+const exitEditProfilePopUp = editProfilePopUp.querySelector('.button_type_exit');
+const nameInput = editProfilePopUp.querySelector('.popup__input_type_name');
+const jobInput = editProfilePopUp.querySelector('.popup__input_type_job');
+
+const elementsItems = document.querySelector('.elements__items');
+const initialCards = [
+  {
+    name:'Гора Эльбрус',
+    link:'./images/elements/elbrus.jpg'
+  },
+  {
+    name:'Ергаки',
+    link:'./images/elements/ergaki.jpg'
+  },
+  {
+    name:'Камчатка',
+    link:'./images/elements/kamchatka.jpg'
+  },
+  {
+    name:'Кижи',
+    link:'./images/elements/kizhi.jpg'
+  },
+  {
+    name:'Урал',
+    link:'./images/elements/ural.jpg'
+  },
+  {
+    name:'Река Волга',
+    link:'./images/elements/volga.jpg'
+  },
+]
+
+
+
+//Функции
 
 function openPopUp() {
   editProfilePopUp.classList.add('popup_opened');
@@ -28,9 +61,22 @@ function editProfile(event) {
   closePopUp();
 }
 
+initialCards.forEach(function(element) {
+  const elementsItemTemplate = document.querySelector('#elementsItem-template').content;
+  const elementsItem = elementsItemTemplate.querySelector('.elements__item').cloneNode(true);
+
+  elementsItem.querySelector('.elements__photo').src = element.link;
+  elementsItem.querySelector('.elements__photo').alt = element.name;
+  elementsItem.querySelector('.elements__title').textContent = element.name;
+
+  console.log(element.name);
+  elementsItems.append(elementsItem);
+});
+
+//Кнопки
+
 editProfileButton.addEventListener('click', openPopUp);
 
 exitEditProfilePopUp.addEventListener('click', closePopUp);
 
 editProfileForm.addEventListener('submit', editProfile);
-
